d37b21f5ddd9127f8d9095c9e9b4c87b2ee18088
zacharykstine <z.k.stine@gmail.com>
2016-10-08 13:31:50 -0500

    Create create_db.sql
    create db

---
+++ create_db.sql
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------------------
+# Zachary K. Stine
+# Started: 
+#     October 8, 2016
+# Last modified: 
+#     October 8, 2016
+# Description: 
+#     Creates tables for storing tweets related to Buddhism and Neo-Paganism.
+# tweet tables store main data (static).
+# hshtg tables store hashtags with the tweet ids the hashtag belongs to.
+# url tables store urls and domains of url with tweet ids they originated from.
+-----------------------------------------------------------------------------*/
+
+CREATE DATABASE twitter_religion;
+
